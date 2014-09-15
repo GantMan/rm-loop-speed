60861859f77424351637cd476681d3b8a95ebfb2
Gant <GantMan@gmail.com>
2014-09-15 10:33:34 -0500

    added 500 test :sparkles:

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -10,6 +10,16 @@ class AppDelegate
       puts "*" * 100
       puts ""
 
+
+      n = 500
+      puts "Testing with n = #{n}"
+      Benchmark.bm do |x|
+        x.report { for i in 1..n; a = "1"; end }
+        x.report { n.times do   ; a = "1"; end }
+        x.report { 1.upto(n) do ; a = "1"; end }
+      end
+      puts "-" * 100
+
       n = 5000
       puts "Testing with n = #{n}"
       Benchmark.bm do |x|
